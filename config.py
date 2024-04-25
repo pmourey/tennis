88ea17a006bb82c3dbe7a56d56a83fcb41b38697
Philippe Mourey <philippe@mourey.com>
2024-04-26 00:43:52 +0200

    ajout classements FFT et chargement fichiers csv licencié(e)s club

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     # TESTING = True
     SECRET_KEY = 'fifa 2022'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///tennis.sqlite3'
-    DEFAULT_CLUB = {'name': 'USC Tennis', 'city': 'Cagnes-sur-Mer'}
+    DEFAULT_CLUB = {'name': 'US CAGNES TENNIS', 'city': 'Cagnes-sur-Mer', 'id': '62 06 0107'}
     PARIS = timezone('Europe/Paris')
 
 
