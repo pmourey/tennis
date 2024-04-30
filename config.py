7936c5b9e297ed2587a0f3884320218f98e2223d
Philippe Mourey <philippe@mourey.com>
2024-04-30 14:29:04 +0200

    Major update: - improved multi-club management using secured cookies - used Blueprint to separate "club" and "championship" modules - Improved navigation between the 2 applications ("_menu.html") - update "select_club" view to load new csv files extracted from Tenup

--- config.py
+++ config.py
@@ -7,7 +7,11 @@ class Config:
     # TESTING = True
     SECRET_KEY = 'fifa 2022'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///tennis.sqlite3'
-    DEFAULT_CLUB = {'name': 'US CAGNES TENNIS', 'city': 'Cagnes-sur-Mer', 'id': '62 06 0107'}
+    CLUBS = [{'name': 'US CAGNES TENNIS', 'city': 'Cagnes-sur-Mer', 'id': '62 06 0107'},
+             {'name': 'TC MOUGINS', 'city': 'Mougins', 'id': '62 06 0108'},
+             {'name': 'ASLM TENNIS CANNES', 'city': 'Cannes', 'id': '62 06 0109'},
+             {'name': 'NICE LAWN TENNIS CLUB', 'city': 'Nice', 'id': '62 06 0110'}
+             ]
     PARIS = timezone('Europe/Paris')
 
 
