9ce2de05b0fcde10913e8b84cc98b53999a8e7a8
Philippe Mourey <philippe.mourey@gmail.com>
2026-05-06 20:44:00 +0200

    Ajout bootstrap + CSS dans blueprint tournament

--- blueprints/tournament/__init__.py
+++ blueprints/tournament/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from flask import Blueprint
 
-tournament_bp = Blueprint('tournament', __name__, template_folder='templates')
+tournament_bp = Blueprint('tournament', __name__, template_folder='templates', static_folder='static')
 
 from . import views
 
